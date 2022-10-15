5c9ff61b68b1084241ea4ce1fedddab3d2f8a9cc
Dzhabrailov63 <petrivanow3344@gmail.com>
2022-10-15 20:30:29 +0400

    Добавьте файлы проекта.

---
+++ Lab1/Source.cpp
@@ -0,0 +1,313 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <ctime>
+#include <omp.h>
+using namespace std;
+
+template<typename T>
+void input_correctly_number(T& aa)
+{
+    while (!(cin >> aa) || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(cin.rdbuf()->in_avail());
+        cout << "Введено неверное значение. Повторите попытку: ";
+    }
+}
+
+void Limitations(int min, int max, int& value)
+{
+    while (value < min || value > max)
+    {
+        cout << "Введенное число не соответствует промежутку. Повторите ввод: ";
+        input_correctly_number(value);
+    }
+}
+
+class Exception {
+private:
+    string str;
+
+public:
+    Exception(const string& Str) : str(Str) {}
+
+    string What() const {
+        return str;
+    }
+};
+
+
+class Matrix {
+private:
+
+    template<typename T>
+    void CorrectlyInput(istream& in, T& aa, const string& exeption_text)
+    {
+        if (!(in >> aa) || cin.peek() != '\n')
+        {
+            /*cin.clear();
+            cin.ignore(cin.rdbuf()->in_avail());
+            throw Exception(exeption_text);*/
+        }
+    }
+
+
+    vector<vector<int>> GenerateMatrix(int row, int col, bool random_generate = false) {
+        vector<vector<int>> v(row);
+        for (int i = 0; i < row; i++) {
+            v[i].resize(col);
+            for (int j = 0; j < col; j++) {
+                if (random_generate) v[i][j] = rand() % 100 + 1;
+            }
+        }
+        return v;
+    }
+
+
+
+public:
+
+    vector<vector<int>> GetMatrixFromFile(const string& path) {
+        ifstream file;
+        file.open(path, ios::in);
+        if (!file.is_open()) throw Exception("Ошибка при открытии файла " + path);
+        int row, col;
+        string line;
+        //CorrectlyInput(file, row, "В файле " + path + " указано некорректное число строк!\n");
+        //CorrectlyInput(file, col, "В файле " + path + " указано некорректное число строк!\n");
+        file >> row >> col;
+        if (row <= 0) throw Exception("Количество строк должно быть больше 0!\n");
+        if (col <= 0) throw Exception("Количество столбцов должно быть больше 0!\n");
+        vector<vector<int>> result = GenerateMatrix(row, col);
+        //getline(file, line, '\n');
+        getline(file, line, '\n');
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                if (j < result[i].size() - 1) getline(file, line, ' ');
+                else getline(file, line, '\n');
+                if (find_if_not(line.begin(), line.end(), ::isdigit) != line.end() || line.size() == 0)
+                    throw Exception("Некорректный элемент в матрице: " + line + "!\n");
+                result[i][j] = stoi(line);
+            }
+        }
+        return result;
+    }
+
+    void WriteMatrixToFile(const vector<vector<int>>& v, const string& path) {
+        ofstream file;
+        file.open(path, ios::out);
+        if (!file.is_open()) throw Exception("Ошибка при открытии файла!\n");
+        file << v.size() << " " << v[0].size() << "\n";
+        for (int i = 0; i < v.size(); i++) {
+            for (int j = 0; j < v[i].size(); j++) {
+                file << v[i][j];
+                if (j < v[i].size() - 1) file << " ";
+            }
+            if (i < v.size() - 1) file << "\n";
+        }
+        file.close();
+    }
+
+    void CreateMatrix(int row, int col, const string& path) {
+        if (row <= 0) throw Exception("Количество строк должно быть больше 0!\n");
+        if (col <= 0) throw Exception("Количество столбцов должно быть больше 0!\n");
+        WriteMatrixToFile(GenerateMatrix(row, col, true), path);
+    }
+
+
+    pair<float, int> SequentialMultiplicateTwoMatrix(const string& path1, const string& path2, const string& result_path) {
+        auto m1 = GetMatrixFromFile(path1);
+        auto m2 = GetMatrixFromFile(path2);
+
+
+
+        if (m1[0].size() != m2.size()) throw Exception("Для перемножения двух матриц количество колонок первой матрицы должно быть равно количеству строк второй матрицы!");
+
+        vector<vector<int>> result = GenerateMatrix(m1.size(), m2[0].size());
+
+        clock_t time = clock();
+
+        int actions_count = 0;
+
+
+        for (int i = 0; i < m1.size(); i++)
+        {
+            for (int k = 0; k < m2[0].size(); k++)
+            {
+                for (int j = 0; j < m1[0].size(); j++)
+                {
+                    result[i][k] += m1[i][j] * m2[j][k];
+                    actions_count++;
+                }
+            }
+        }
+
+        time = clock() - time;
+
+        //cout << "Время выполнения умножения матриц: " << (double)time / CLOCKS_PER_SEC << " секунд\n" << "Количество выполненный действий: " << actions_count << "!\n";
+
+        WriteMatrixToFile(result, result_path);
+
+        return pair<float, int>((double)time / CLOCKS_PER_SEC, actions_count);
+    }
+
+    pair<float, int> ParallelMultiplicateTwoMatrixByOpenMp(const string& path1, const string& path2, const string& result_path, const int option = 1) {
+        auto m1 = GetMatrixFromFile(path1);
+        auto m2 = GetMatrixFromFile(path2);
+
+
+
+        if (m1[0].size() != m2.size()) throw Exception("Для перемножения двух матриц количество колонок первой матрицы должно быть равно количеству строк второй матрицы!");
+
+        vector<vector<int>> result = GenerateMatrix(m1.size(), m2[0].size());
+
+        clock_t time = clock();
+
+        int actions_count = 0;
+
+        if (option == 1) {
+
+            for (int i = 0; i < m1.size(); i++)
+            {
+                for (int k = 0; k < m2[0].size(); k++)
+                {
+#pragma omp for
+                    for (int j = 0; j < m1[0].size(); j++)
+                    {
+                        result[i][k] += m1[i][j] * m2[j][k];
+                        actions_count++;
+                    }
+
+                }
+            }
+
+        }
+        //3.6318
+        //6.3748
+        //12.7894
+        //18.7246
+        //25.0028
+
+        else if (option == 2) {
+            for (int i = 0; i < m1.size(); i++)
+            {
+#pragma omp for
+                for (int k = 0; k < m2[0].size(); k++)
+                {
+                    for (int j = 0; j < m1[0].size(); j++)
+                    {
+                        result[i][k] += m1[i][j] * m2[j][k];
+                        actions_count++;
+                    }
+
+                }
+            }
+        }
+
+        else if (option == 3) {
+#pragma omp for
+            for (int i = 0; i < m1.size(); i++)
+            {
+                for (int k = 0; k < m2[0].size(); k++)
+                {
+                    for (int j = 0; j < m1[0].size(); j++)
+                    {
+                        result[i][k] += m1[i][j] * m2[j][k];
+                        actions_count++;
+                    }
+
+                }
+            }
+        }
+
+        else if (option == 4) {
+
+#pragma omp parallel 
+            {
+                for (int i = 0; i < m1.size(); i++)
+                {
+                    for (int k = 0; k < m2[0].size(); k++)
+                    {
+                        for (int j = 0; j < m1[0].size(); j++)
+                        {
+                            result[i][k] += m1[i][j] * m2[j][k];
+                            actions_count++;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        else if (option == 5) {
+#pragma omp parallel for schedule (static)
+            for (int i = 0; i < m1.size(); i++)
+            {
+                for (int k = 0; k < m2[0].size(); k++)
+                {
+                    for (int j = 0; j < m1[0].size(); j++)
+                    {
+                        result[i][k] += m1[i][j] * m2[j][k];
+                        actions_count++;
+                    }
+
+                }
+            }
+        }
+
+        else throw Exception("Указанная опция недоступна!");
+
+
+        time = clock() - time;
+
+        //WriteMatrixToFile(result, result_path);
+
+        return pair<float, int>((double)time / CLOCKS_PER_SEC, actions_count);
+    }
+};
+
+
+
+void CreateMatrix() {
+    int row, col;
+    string path;
+    cout << "Введите количество строк: ";
+    input_correctly_number(row);
+    cout << "Введите количество колонок: ";
+    input_correctly_number(col);
+    cout << "Введите путь для записи сгенерированной матрицы в файл: ";
+    cin >> path;
+    Matrix().CreateMatrix(row, col, path);
+}
+
+
+
+
+
+
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    cout << "Последовательное умножение: \n\n\n";
+    double sum = 0;
+    int actions_count = 0, start = 500, finish = 1000;
+
+    for (int i = start; i < finish; i += 100) {
+
+        for (int j = 0; j < 5; j++) {
+            Matrix().CreateMatrix(i, i, "m1.txt");
+            //auto result = Matrix().SequentialMultiplicateTwoMatrix("m1.txt", "m1.txt", "m1.txt");
+            auto result = Matrix().ParallelMultiplicateTwoMatrixByOpenMp("m1.txt", "m1.txt", "m1.txt");
+            sum += result.first;
+            actions_count = result.second;
+        }
+        cout << i << " * " << i << ": " << double(sum / 5) << " seconds, " << actions_count << " actions\n";
+        sum = 0;
+        actions_count = 0;
+    }
+
+    cout << "\n\n\n";
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,69 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		void WriteMatrixToFile(const vector<vector<int>>& m1, const vector<vector<int>>& m2) {
+			Matrix().WriteMatrixToFile(m1, "m1.txt");
+			Matrix().WriteMatrixToFile(m2, "m2.txt");
+		}
+
+		void ProcessSequentialMultiplicating(const vector<vector<int>>& m1, const vector<vector<int>>& m2) {
+			WriteMatrixToFile(m1, m2);
+			Matrix().SequentialMultiplicateTwoMatrix("m1.txt", "m2.txt", "m3.txt");
+		}
+
+		void ProcessParallelMultiplicating(const vector<vector<int>>& m1, const vector<vector<int>>& m2, const int option) {
+			WriteMatrixToFile(m1, m2);
+			Matrix().ParallelMultiplicateTwoMatrixByOpenMp("m1.txt", "m2.txt", "m3.txt", option);
+		}
+
+		void Multiplicate(const vector<vector<int>>& m1, const vector<vector<int>>& m2, const vector<vector<int>>& result) {
+			ProcessSequentialMultiplicating(m1, m2);
+			Assert::IsTrue(Matrix().GetMatrixFromFile("m3.txt") == result);
+			for (int i = 1; i < 6; i++) {
+				ProcessParallelMultiplicating(m1, m2, i);
+				Assert::IsTrue(Matrix().GetMatrixFromFile("m3.txt") == result);
+			}
+		}
+
+		void ThrowException(const vector<vector<int>>& m1, const vector<vector<int>>& m2) {
+			bool check = false;
+			try {
+				ProcessSequentialMultiplicating(m1, m2);
+			}
+			catch (const Exception& ex) {
+				check = true;
+			}
+			Assert::IsTrue(check);
+		}
+
+
+
+
+
+
+
+
+
+		TEST_METHOD(CheckMultiplicate)
+		{
+			Multiplicate({ {3,3,3}, {3,3,3}, {3,3,3} }, { {9,9,9}, {9,9,9}, {9,9,9} }, { {81, 81, 81}, {81, 81, 81}, {81, 81, 81} });
+			Multiplicate({ {2,2}, {2,2} }, { {3,3}, {3,3}, }, { {12, 12}, {12, 12} });
+			Multiplicate({ {42,68,35,1,70}, {25,79,59,63,65}, {6,46,82,28,62} }, { {92, 96, 43}, {28, 37, 92}, {5, 3, 54}, {93, 83, 22}, {17, 19, 96} }, { {7226, 8066, 16694}, {11771, 11964, 19155}, {5908, 6026, 15486} });
+
+		}
+
+		TEST_METHOD(CheckThrowException) {
+			ThrowException({ {1}, }, { {2,2}, {2,2} });
+			ThrowException({ {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1} }, { {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1}, {1,1,1,1,1} });
+		}
+	};
+}
